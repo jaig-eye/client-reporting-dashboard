238dc2b47d2cd03b7964ead63db39115e5f33451
Robert <robert@golaunchlocal.com>
2026-03-24 11:45:20 -0400

    fix: bump Google Ads API from v16 (sunset) to v17

--- src/lib/google-ads.ts
+++ src/lib/google-ads.ts
@@ -1,4 +1,4 @@
-const API_VERSION = 'v16'
+const API_VERSION = 'v17'
 const BASE_URL = `https://googleads.googleapis.com/${API_VERSION}`
 
 export async function refreshGoogleToken(refreshToken: string): Promise<string> {
